f6f32ea109b555f13d53d0da6cf9147ae276487e
George Rivera <GRivera.br@gmail.com>
2018-05-14 12:35:03 -0400

    added: unminified version

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    entry: './src/evolvcfs.js',
+    output: {
+        path: path.resolve(__dirname, 'lib'),
+        filename: 'evolvcfs.min.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env'],
+                    plugins: ['transform-es3-member-expression-literals', 'transform-es3-property-literals']
+
+                }
+            }
+        }]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
